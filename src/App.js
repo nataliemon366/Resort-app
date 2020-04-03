d12bbb7760bb22dcb63f744ee86eac99df1f7b02
Natalie Montano <nat.montano366@gmail.com>
2020-04-02 17:48:20 -0700

    set up components folder and started on Navbar

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ import Error from "./pages/Error"
 
 import {Route, Switch} from 'react-router-dom';
 
-
+import Navbar from "./components/Navbar";
 function App(){
   return (
     <>
+    <Navbar/>
+    <Switch>
     <Route exact path="/" component={Home}/>
     <Route exact path="/rooms/" component={Rooms}/>
-    <Route exact path="/single-room/" component={SingleRoom}/>
-    <Route exact path="/error/" component={Error}/>
-   
+    <Route exact path="/rooms/:slug" component={SingleRoom}/>
+    <Route component={Error}/>
+    </Switch>
     </>
   )
 }
